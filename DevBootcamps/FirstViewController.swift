6077a19e4722ee919b508d8f64e580eab3769bc2
your name <your@email.com>
2016-02-02 21:49:09 +0100

    Initial Commit

---
+++ DevBootcamps/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DevBootcamps
+//
+//  Created by Tim on 02.02.16.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
